239b8dd058e6ab884c3167d76d385902afa79a3c
Srivatsan <vatsan1993@gmail.com>
2024-09-17 11:41:13 +0530

    finished basic part

--- server/app.js
+++ server/app.js
@@ -18,13 +18,33 @@ const {
   getSongBySongId,
   addSongByAlbumId,
   editSongBySongId,
-  deleteSongBySongId
+  deleteSongBySongId,
 } = require('./data');
 
 const express = require('express');
 const app = express();
 
 // Your code here
+// parse json
+app.use(express.json());
+
+// display the request body for each request
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+app.get('/artists', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.status(200).json(getAllArtists());
+});
+
+app.post('/artists', (req, res) => {
+  let data = Object.assign({}, req.body);
+  data = addArtist(data);
+  res.set('Content-Type', 'application/json');
+  res.status(201).json(data);
+});
 
 // DO NOT MODIFY
 if (require.main === module) {
